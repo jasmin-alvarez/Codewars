fe30158fc9d0249ea555e8ff00979245e091a0c9
Jasmin-L-Alvarez <ajasmin01@gmail.com>
2021-02-13 17:01:06 -0500

    adding more

---
+++ returnNegative.js
@@ -0,0 +1,19 @@
+// Return Negative 
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+
+
+
+
+
+function makeNegative(num){
+  if (num>0){
+    return -num
+  }
+  else if (num<0){
+    return num
+  }
+  else {
+    return 0
+  }
+}
