63cafd4ff9f4e9c7602ed15fc1d5c52d6a9db835
Jasmin-L-Alvarez <ajasmin01@gmail.com>
2021-02-22 17:24:09 -0500

    multiplyCodeWar

---
+++ Multiply.js
@@ -0,0 +1,11 @@
+// This code does not execute properly. Try to figure out why.
+// function multiply(a, b){
+//   a * b
+// }
+
+function multiply(a, b){
+  console.log(a * b)
+  return a*b;
+}
+
+
