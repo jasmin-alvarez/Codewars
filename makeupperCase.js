11bf676fe85b80069a90a3c43dc98fc469317bc6
Jasmin-L-Alvarez <ajasmin01@gmail.com>
2021-02-25 17:21:38 -0500

    2/25/2021

---
+++ makeupperCase.js
@@ -0,0 +1,9 @@
+// Write a function which converts the input string to uppercase.
+
+
+function makeUpperCase(str) {
+  let outcome = str.toUpperCase()
+  console.log(outcome)
+   return outcome 
+ }
+
