b5b4b38cf0b7402d8c187fcb3d3cdbec281d37d5
Jasmin-L-Alvarez <ajasmin01@gmail.com>
2021-04-06 23:17:16 -0400

    4/6/2021

---
+++ Generate range of integers.js
@@ -0,0 +1,16 @@
+// Implement a function named generateRange(min, max, step), which 
+// takes three arguments and generates a range of integers from min to max, 
+// with the step. The first integer is the minimum value, the second is the 
+// maximum of the range and the third is the step. (min < max)
+
+
+function generateRange(min, max, step) {
+  const result = [];
+  for (let i = min; i <= max; i += step) {
+    result.push(i)
+  }
+  return result
+}
+
+
+
