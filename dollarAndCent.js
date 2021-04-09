36f0dfec2f4830cdbb2f629eb170ebf86e811e27
Jasmin-L-Alvarez <ajasmin01@gmail.com>
2021-04-08 22:33:17 -0400

    4/8/2021

---
+++ dollarAndCent.js
@@ -0,0 +1,26 @@
+
+// The company you work for has just been awarded a contract to build a payment gateway. In order to help move things along, you have volunteered to create a function that will take a float and return the amount formatting in dollars and cents.
+
+// 39.99 becomes $39.99
+
+// The rest of your team will make sure that the argument is sanitized before being passed to your function although you will need to account for adding trailing zeros if they are missing (though you won't have to worry about a dangling period).
+
+// Examples:
+
+// 3 needs to become $3.00
+
+// 3.1 needs to become $3.10
+
+
+
+// /RETURN : with dollar sign integar 
+// EXAMPLE :  3 ---> $3.00
+//PSEUDO: gonna make a function that takes in argument of amount
+// Convert a number into a string, rounding the number to keep only two decimals:
+// using .toFIX
+
+
+
+function formatMoney(amount){
+  return '$' + amount.toFixed(2);
+}
